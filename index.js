8b83ebbc7ef1c143cdaacfdfaac1c6ba7202f1bb
Matthew Brophy <brophy3030@gmail.com>
2018-10-18 12:42:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return "Welcome, " + name + ". You are number " + line.length + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return "Currently serving " + katzDeliLine.shift(0) + ".";
+
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+  }
+
+
+
+function currentLine(line) {
+  let newArray = [];
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    for (let i = 0; i <line.length; i++) {
+    newArray.push (" " + `${i + 1}. ${line[i]}`)
+  }
+  }
+  return "The line is currently:" + newArray;
+}
